250b71976d128c50982bd55d51b22af4e086dd2c
Greenfire44Official <48894984+Greenfire44Official@users.noreply.github.com>
2025-08-19 00:47:01 -0600

    Made a generic parse function. Added nearly all cs50 c functions for parity, but they all use the generic get_input() under the hood. The only function that actually does something more is get_u32()

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 /// Gets a string
 pub fn get_string(message: &str) -> String {
     println!("{message}");
@@ -11,28 +13,41 @@ pub fn get_string(message: &str) -> String {
     input.trim().to_string()
 }
 
-/// Gets a char. Will try again until user inputs a valid char.
-pub fn get_char(message: &str) -> char {
+/// Asks for an input using the provided message and will keep asking
+/// until the input can successfully be parsed into the type this function gets casted to.
+pub fn get_input<T: FromStr>(message: &str) -> T {
     loop {
         let input = get_string(message);
-        if input.chars().count() == 1 {
-            return input.chars().next().unwrap();
-        }
+        match input.trim().parse() {
+            Ok(num) => return num,
+            Err(_) => continue,
+        };
     }
+}
+
+/// Gets a char. Will try again until user inputs a valid char.
+pub fn get_char(message: &str) -> char {
+    get_input(message)
 }
 
 /// Gets an integer (i32). Will try again until user inputs a valid integer.
 pub fn get_i32(message: &str) -> i32 {
-    let output: i32;
-    loop {
-        let input = get_string(message);
-        output = match input.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-        break;
-    }
-    output
+    get_input(message)
+}
+
+/// Gets an integer (i64). Will try again until user inputs a valid integer.
+pub fn get_i64(message: &str) -> i64 {
+    get_input(message)
+}
+
+/// Gets an float (f32). Will try again until user inputs a valid float.
+pub fn get_f32(message: &str) -> f32 {
+    get_input(message)
+}
+
+/// Gets an float (f64). Will try again until user inputs a valid float.
+pub fn get_f64(message: &str) -> f64 {
+    get_input(message)
 }
 
 /// Gets an unsigned integer (u32). Will try again until user inputs a valid integer.

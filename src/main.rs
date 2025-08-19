62dfcff073bf7f256c9d9008166bc475147f797e
Greenfire44Official <48894984+Greenfire44Official@users.noreply.github.com>
2025-08-19 16:17:41 -0600

    mario_more from cs50 PSET1

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use my_library::get_input;
+const BLOCK: char = '#';
+const SPACE: char = ' ';
+const GAP_WIDTH: u8 = 2;
+const MIN_HEIGHT: u8 = 1;
+const MAX_HEIGHT: u8 = 8;
+
+fn main() {
+    let mut height: u8;
+    loop {
+        height = get_input("Pyramid Height: ");
+        match height{
+            _ if height > MAX_HEIGHT => println!("Pyramid too big! ({height}). Max height: 8"),
+            _ if height < MIN_HEIGHT => println!("So, no pyramid?"),
+            _ => break
+        }
+    }
+    print_pyramid(height)
+}
+
+fn print_pyramid(height: u8) {
+    // if step > height {
+    //     return;
+    // }
+    // print_pyramid(height, step + 1);
+    for step in (0..height).rev() {
+        for i in 0..height {
+            if i < step {
+                print!("{SPACE}")
+            } else {
+                print!("{BLOCK}")
+            }
+        }
+        for _ in 0..GAP_WIDTH {
+            print!("{SPACE}")
+        }
+        for _ in 0..height - step {
+            print!("{BLOCK}")
+        }
+        println!("")
+    }
+}

69f7c2213d2dae8d88b733021db90ce16304956b
Greenfire44Official <48894984+Greenfire44Official@users.noreply.github.com>
2025-08-19 19:04:19 -0600

    Re-structured to make get_input() the "main" input handling function that relies on no other function. get_string() now relies on get_input().

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,39 @@
-use std::{io::{self, Write}, str::FromStr};
-
-/// Gets a string
-pub fn get_string(message: &str) -> String {
-    print!("{message}");
-    io::stdout().flush().expect("I/O error");
-
-    let mut input = String::new();
-
-    std::io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
-
-    input.trim().to_string()
-}
+use std::{
+    io::{self, Write},
+    str::FromStr,
+};
 
 /// Asks for an input using the provided message and will keep asking
 /// until the input can successfully be parsed into the type this function gets casted to.
 pub fn get_input<T: FromStr>(message: &str) -> T {
     loop {
-        let input = get_string(message);
-        match input.trim().parse() {
-            Ok(num) => return num,
+        print!("{message}");
+        io::stdout().flush().expect("I/O error");
+
+        let mut input = String::new();
+
+        std::io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        input = input.trim().to_string();
+
+        match input.parse() {
+            Ok(output) => return output,
             Err(_) => continue,
-        };
+        }
     }
+}
+
+/// Gets an unsigned integer (u32). Will try again until user inputs a valid integer.
+/// If a negative integer is inputted it returns the absolute value of the number.
+pub fn get_u32(message: &str) -> u32 {
+    get_i32(message).abs() as u32
+}
+
+/// Gets a string
+pub fn get_string(message: &str) -> String {
+    get_input(message)
 }
 
 /// Gets a char. Will try again until user inputs a valid char.
@@ -49,10 +59,4 @@ pub fn get_f32(message: &str) -> f32 {
 /// Gets an float (f64). Will try again until user inputs a valid float.
 pub fn get_f64(message: &str) -> f64 {
     get_input(message)
-}
-
-/// Gets an unsigned integer (u32). Will try again until user inputs a valid integer.
-/// If a negative integer is inputted it returns the absolute value of the number.
-pub fn get_u32(message: &str) -> u32 {
-    get_i32(message).abs() as u32
 }

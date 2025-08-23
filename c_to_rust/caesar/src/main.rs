ab1a5aae563711be2e9498ffd1b5abfa4b34a75d
Greenfire44Official <48894984+Greenfire44Official@users.noreply.github.com>
2025-08-23 15:42:38 -0600

    caesar

---
+++ c_to_rust/caesar/src/main.rs
@@ -0,0 +1,38 @@
+use std::env::args;
+
+use anyhow::Result;
+use my_library::get_string;
+
+fn main() -> Result<()> {
+    let args = args().collect::<Vec<String>>();
+    if args.len() != 2 {
+        println!("");
+        anyhow::bail!("Invalid input\n\nUsage: {} key\n", args[0]);
+    }
+    let key: u128 = match args[1].parse() {
+        Ok(k) => k,
+        Err(_) => anyhow::bail!("Key should be a number"),
+    };
+    let key: u8 = (key % 26) as u8;
+
+    let output: String = get_string("Text to encrypt: ")
+        .chars()
+        .filter_map(|c| caesar_encrypt(key, c))
+        .collect();
+    println!("ciphertext: {output}");
+    Ok(())
+}
+
+fn caesar_encrypt(key: u8, c: char) -> Option<char> {
+    match c {
+        c if c.is_alphabetic() => {
+            let a: u8 = match c.is_lowercase() {
+                true => 'a' as u8,
+                false => 'A' as u8,
+            };
+            let c = c as u8;
+            Some(((c - a + key) % 26 + a) as char)
+        }
+        _ => Some(c),
+    }
+}
